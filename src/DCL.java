f213f26ff8e0544c9b9846db303052418af02952
emmschuster <emmschuster@tsn.at>
2020-05-18 15:34:48 +0200

    Zeitvergleich mit der Java Implementierung fertig (leider ist meine langsamer)

--- src/DCL.java
+++ src/DCL.java
@@ -16,6 +16,7 @@ public class DCL {
 		}
 		return length;
 	}
+
 	private Elem getElemByIndex(int index) {
 		Elem currentElem = head;
 		int counter = 0;
@@ -23,7 +24,7 @@ public class DCL {
 			currentElem = currentElem.next;
 			counter++;
 			if (currentElem == null) {
-				System.out.println("No Element with Index: " + index + " exists!");
+				System.out.println("Es gibt kein Element mit dem Index: "+ index);
 				return null;
 			}
 		}
@@ -63,45 +64,96 @@ public class DCL {
 	
 	public void deleteElem(int index) {
 		if (index < 0) {
-			System.out.println("positiver Wert muss eingegeben werden!!!");
+			System.out.println("Positive index (0 included) expected!");
 			return;
 		}
 		if (index >= length()) {
-			System.out.println("der index ist zu groß; liste is zu klein/kurz");
+			System.out.println("Index out of Bounds!");
 			return;
 		}
+		//wenn head loeschen -> naechtes element wird head
 		if (index == getIndexByElem(head)) { 
 			head = head.next;
 			return;
 		}
+		//wenn tail loeschen -> vorheriges element wird tail
 		if (index == getIndexByElem(tail)) {
 			tail = tail.last;
 			return;
 		}
+		//sonnst muessen sich beider ändern
 		Elem deleteElem = getElemByIndex(index);
 		Elem lastElem = deleteElem.last;
 		Elem nextElem = deleteElem.next;
 		lastElem.next = nextElem;
 		nextElem.last = lastElem;
 	}
-	
+	public void deleteHead() {
+		deleteElem(getIndexByElem(head));
+	}
+	public void deleteTail() {
+		deleteElem(getIndexByElem(tail));
+	}
 	public void swap(int index1, int index2) {
 		if (index1 == index2) {
 			System.out.println("Nothing to swap here!");
 			return;
 		}
+		if (index1 > index2) { //index1 must be < index2
+			int temp = index1;
+			index1 = index2;
+			index2 = temp;
+		}
 		Elem e1 = getElemByIndex(index1);
 		Elem e2 = getElemByIndex(index2);
-		Elem temp = e1.next;
-		e1.next = e2.next;
-		e2.next = temp;
-		temp = e1.last;
-		e1.last = e2.last;
-		e2.last = temp;
-		e1.last.last = e2;
-		e2.next.next = e1;
-		e2.last.next = e2;
-		e1.next.last = e1;
+		if (e1 == null || e2 == null) {
+			System.out.println("Index out of Bounds!");
+			return;
+		}
+		if (e1 == head) {
+			head = e2;
+		}
+		if (e2 == tail) {
+			tail = e1;
+		}
+		// wenn nebeneinnander
+		if (e1.next == e2) { 
+			e1.next = e2.next;
+			e2.last = e1.last;
+			
+			if (e1.next != null) {
+				e1.next.last = e1;
+			}
+			if (e2.last != null) {
+				e2.last.next = e2;
+			}
+			
+			e2.next = e1;
+			e1.last = e2;
+		} else {
+			Elem l = e2.last;
+			Elem n = e2.next;
+			
+			e2.last = e1.last;
+			e2.next = e1.next;
+			
+			e1.last = l;
+			e1.next = n;
+			
+			if (e2.next != null) {
+				e2.next.last = e2;
+			}
+			if (e2.last != null) {
+				e2.last.next = e2;
+			}
+			
+			if (e1.next != null) {
+				e1.next.last = e1;
+			}
+			if (e1.last != null) {
+				e1.last.next = e1;
+			}
+		}
 	}
 	public void printList() {
 		Elem currentElem = head;
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,58 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc= new Scanner (System.in);
+		System.out.println("Willst du...");
+		System.out.println("... das Programm mit der DVL Liste starten? Dann 1");
+		System.out.println("... Das DVL Program mit der DVL von Java vergleichen? Dann 2");
+		int antwort = sc.nextInt();
+		switch (antwort) {
+		case 1:
+			option1();
+			break;
+		case 2:
+			option2();
+			break;
+		}
+		sc.close();
+	}
+	public static void option2() {
+		System.out.println("Zeitmessung Java DVK 1000 Durchgänge (erstellen, 4 hinzufügen, "
+				+ "letztes loeschen, 2. Stelle loeschen)");
+		long t1=System.nanoTime();
+		for (int i = 0; i < 1000; i++) {
+			LinkedList<Integer>JavaListe=new LinkedList<Integer>();
+			JavaListe.add(2);
+			JavaListe.add(4);
+			JavaListe.add(6);
+			JavaListe.add(2, 4);
+			JavaListe.removeLast();
+			JavaListe.remove(2);
+		}
+		long t2=System.nanoTime();
+		System.out.println(t2-t1+"ns hat es gedauert.");
+		
+		System.out.println("Zeitmessung Meine DVK, 1000 Durchgänge (erstellen, 4 hinzufügen, "
+				+ "letztes loeschen, 2. Stelle loeschen)");
+		long t3=System.nanoTime();
+		for (int i = 0; i < 1000; i++) {
+			DCL l = new DCL(10);
+			l.addElem(2);
+			l.addElem(4);
+			l.addElem(6);
+			l.addElem(2, 4);
+			l.deleteTail();
+			l.deleteElem(2);
+		}
+		long t4=System.nanoTime();
+		System.out.println(t4-t3+"ns hat es gedauert.");
+		
+		System.out.println("\nDie Java Implementierung ist um "+((t4-t3)-(t2-t1))+"ns schneller.");
+	}
+	public static void option1() {	//letztes element löscheln... da haggelts no
 		DCL ll = new DCL(10);
 		ll.addElem(1);
 		ll.addElem(2);
@@ -12,25 +64,19 @@ public class Main {
 		System.out.println("Länge der Liste: " + ll.length());
 		ll.printList();
 		System.out.println("Stellen mit dem index 1 und 2 werden getauscht: ");
-		ll.swap(4, 5);
+		ll.swap(1, 2);
 		ll.printList();
-		System.out.println("Das Element mit index 5 wird geschlöscht also 22");
-		ll.deleteElem(5); 
+		System.out.println("Das Element mit index 4 wird geloescht");
+		ll.deleteElem(4); 
 		System.out.println("Jetzige Länge: " + ll.length());
 		ll.printList();
-		System.out.println("Das Element mit index 0 wird geschlöscht (also es erste)");
-		ll.deleteElem(0); 
+		System.out.println("Das erste Element wird geloescht");
+		ll.deleteHead(); 
 		System.out.println("Jetzige Länge: " + ll.length());
 		ll.printList();
-		System.out.println("Das letzte Element wird geschöscht also mit dem Index "+ll.length());
-		ll.deleteElem(ll.length()-1); 
+		System.out.println("Das letzte Element wird geschöscht");
+		ll.deleteTail(); 
 		System.out.println("Jetzige Länge: " + ll.length());
 		ll.printList();
 	}
-
 }
-/*
- * o	Erstes head und letztes Tail; ein Element hat Adresse vom nächsten und vom vorherigen a; jedes Element hat 2 Adressen also 2 pointer
-o	Länge, Ausgabe mit Adressen, Eingabe also hinzufügen, DELETE, und dann noch SWAP dieses Mal
-o	Delete und swap brauchen indes zum welchen die löschen oder welche sie tauschen wollen  
-*/
