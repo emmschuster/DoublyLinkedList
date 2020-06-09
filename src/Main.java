868a493049b1d4c4c163eb14a298d8aea85366fd
emmschuster <emmschuster@tsn.at>
2020-06-09 13:59:53 +0200

    Option3 mit neuem vergleichen angefangen; ohne Hashmaps jedoch

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.LinkedList;
@@ -8,6 +7,10 @@ import java.util.Scanner;
 
 public class Main {
 	static String stempel;
+	static long t1,t2,t3,t4;
+	static DCL ll = new DCL(10);
+	static LinkedList<Integer>JavaListe2=new LinkedList<Integer>();
+
 	public static void main(String[] args) {
 		Scanner sc= new Scanner (System.in);
 		DateTimeFormatter date = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
@@ -15,7 +18,9 @@ public class Main {
 		stempel = date.format(now);
 		System.out.println("Willst du...");
 		System.out.println("... das Programm mit der DVL Liste starten? Dann 1");
-		System.out.println("... Das DVL Program mit der DVL von Java vergleichen? Dann 2");
+		System.out.println("... Das DVL Program mit der DVL von Java vergleichen (allgemein durchschnitt zeit) ? Dann 2");
+		System.out.println("... wieder vergleichen, aber diesmal die einzelnen Vorgänge? Dann 3");
+
 		int antwort = sc.nextInt();
 		switch (antwort) {
 		case 1:
@@ -24,13 +29,102 @@ public class Main {
 		case 2:
 			option2();
 			break;
+		case 3:
+			option3();
+			break;
 		}
 		sc.close();
+		
+	}
+	private static void option3() {
+		ll.addElem(1);
+		ll.addElem(2);
+		ll.addElem(3);
+		ll.addElem(11);
+		ll.addElem(22);
+		ll.addElem(33);
+		JavaListe2.add(1);
+		JavaListe2.add(2);
+		JavaListe2.add(3);
+		JavaListe2.add(11);
+		JavaListe2.add(22);
+		JavaListe2.add(33);
+		laenge();
+		addElementSchluss();
+		addElementBestimmteStelle();
+		deleteDenIndex();
+		deleteFirst();
+		deleteLast();
+//		tauschen();
+	}
+	/*private static void tauschen() {
+		t1=System.nanoTime();
+		ll.swap(2, 4);
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.
+		t4=System.nanoTime();
+		System.out.println("Anzeigen der Liste (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+		
+	}*/
+	private static void deleteLast() {
+		t1=System.nanoTime();
+		ll.deleteTail();
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.removeLast();
+		t4=System.nanoTime();
+		System.out.println("Letztes Element loeschen (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+	}
+	private static void deleteFirst() {
+		t1=System.nanoTime();
+		ll.deleteHead();
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.removeFirst();
+		t4=System.nanoTime();
+		System.out.println("Erstes Element loeschen (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+	}
+	private static void deleteDenIndex() {		//deleteElem vs remove
+		t1=System.nanoTime();
+		ll.deleteElem(4);
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.remove(4);
+		t4=System.nanoTime();
+		System.out.println("Element beliebige Stelle loeschen (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+	}
+	private static void addElementBestimmteStelle() {
+		t1=System.nanoTime();
+		ll.addElem(1001, 4);
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.add(4, 1001);
+		t4=System.nanoTime();
+		System.out.println("Element beliebige Stelle hinzufuegen (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+	}
+	private static void addElementSchluss() {
+		t1=System.nanoTime();
+		ll.addElem(1000);
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.add(1000);
+		t4=System.nanoTime();
+		System.out.println("Element hinten hinzufuegen (Dauer) : Meine DVL "+(t2-t1)+"s		Javas Linked List "+(t4-t3));
+	}
+	private static void laenge() {		//length vs size
+		t1=System.nanoTime();
+		ll.length();
+		t2=System.nanoTime();
+		t3=System.nanoTime();
+		JavaListe2.size();
+		t4=System.nanoTime();
+		System.out.println("Laenge herausfinden (Dauer) : Meine DVL "+(t2-t1)+"s 		 Javas Linked List "+(t4-t3));
 	}
 	public static void option2() {
 		System.out.println("Zeitmessung Java DVK 1000 Durchgänge (erstellen, 4 hinzufügen, "
 				+ "letztes loeschen, 2. Stelle loeschen)");
-		long t1=System.nanoTime();
+		t1=System.nanoTime();
 		for (int i = 0; i < 1000; i++) {
 			LinkedList<Integer>JavaListe=new LinkedList<Integer>();
 			JavaListe.add(2);
@@ -40,12 +134,12 @@ public class Main {
 			JavaListe.removeLast();
 			JavaListe.remove(2);
 		}
-		long t2=System.nanoTime();
+		t2=System.nanoTime();
 		System.out.println(t2-t1+"ns hat es gedauert.");
 		
 		System.out.println("Zeitmessung Meine DVK, 1000 Durchgänge (erstellen, 4 hinzufügen, "
 				+ "letztes loeschen, 2. Stelle loeschen)");
-		long t3=System.nanoTime();
+		t3=System.nanoTime();
 		for (int i = 0; i < 1000; i++) {
 			DCL l = new DCL(10);
 			l.addElem(2);
@@ -55,28 +149,23 @@ public class Main {
 			l.deleteTail();
 			l.deleteElem(2);
 		}
-		long t4=System.nanoTime();
+		t4=System.nanoTime();
 		System.out.println(t4-t3+"ns hat es gedauert.");
 		
 		System.out.println("\nDie Java Implementierung ist um "+((t4-t3)-(t2-t1))+"ns schneller.");
-		
-		 FileWriter pWriter = null;
-	        try {
-//	        	Calendar cal = Calendar.getInstance ();
-//	        	String uhrzeit= cal.get( Calendar.HOUR_OF_DAY ) + ":" +
-//	                      cal.get( Calendar.MINUTE ) + ":" +
-//	                      cal.get( Calendar.SECOND ) + ":" +
-//	                      cal.get( Calendar.MILLISECOND ) ;
-//	        	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-//	        	timestamp=timestamp;
-	            pWriter = new FileWriter("Emma"+stempel+".txt");
-	            System.out.println("Die Java Implementierung ist um "+((t4-t3)-(t2-t1))+"ns schneller.");
-	        } catch (IOException ioe) {
-	            ioe.printStackTrace();
-	        } 
+		insFileSchreiben(); 
 	}
-	public static void option1() {	//letztes element löscheln... da haggelts no
-		DCL ll = new DCL(10);
+	private static void insFileSchreiben() {
+		FileWriter pWriter = null;
+        try {
+            pWriter = new FileWriter("Emma"+stempel+".txt");
+            System.out.println("Die Java Implementierung ist um "+(((t4-t3)-(t2-t1))/1000)+"mikro s schneller.");
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+		
+	}
+	public static void option1() {		//letztes element löscheln... da haggelts no
 		ll.addElem(1);
 		ll.addElem(2);
 		ll.addElem(3);
